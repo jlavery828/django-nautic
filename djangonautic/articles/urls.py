464c93cefc9dc0f45773ab6ea610f19ffa4b8ae1
Jason <jlavery828@gmail.com>
2018-12-13 12:55:00 -0500

    added accounts functionality

--- djangonautic/articles/urls.py
+++ djangonautic/articles/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
+app_name = 'articles'
+
 urlpatterns = [
-    path('', views.article_list),
+    path('', views.article_list, name="list"),
+    path('create/', views.article_create, name="create"),
+    path('<slug:slug>/', views.article_detail, name="detail"),
 ]
